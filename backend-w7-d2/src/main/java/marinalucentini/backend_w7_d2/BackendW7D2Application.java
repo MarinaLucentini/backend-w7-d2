a0f21f1617b42a90345f28b8237f2c92686aa63c
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-07-02 14:05:06 +0200

    configuration project

---
+++ backend-w7-d2/src/main/java/marinalucentini/backend_w7_d2/BackendW7D2Application.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w7_d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendW7D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendW7D2Application.class, args);
+	}
+
+}
---
+++ backend-w7-d2/src/test/java/marinalucentini/backend_w7_d2/BackendW7D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w7_d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendW7D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
